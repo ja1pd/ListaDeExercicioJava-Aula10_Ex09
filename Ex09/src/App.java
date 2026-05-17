4a05fb1228c39afca813f1f2e6d7b3ae32f853b8
ja1pd <159259175+ja1pd@users.noreply.github.com>
2026-05-17 14:37:33 -0300

    Update App.java

--- Ex09/src/App.java
+++ Ex09/src/App.java
@@ -13,20 +13,14 @@ public class App {
         item1.preco = scanner.nextDouble();
         scanner.nextLine();
 
-        System.out.println("Deseja adicionar um 2º item? (s/n)");
-        String resposta = scanner.nextLine();
-
-        Hamburguer item2 = null;
-        if (resposta.equalsIgnoreCase("s")) {
-            item2 = new Hamburguer();
-            System.out.println("Nome do 2º hambúrguer:");
-            item2.nome = scanner.nextLine();
-            System.out.println("Descrição:");
-            item2.descricao = scanner.nextLine();
-            System.out.println("Preço:");
-            item2.preco = scanner.nextDouble();
-            scanner.nextLine();
-        }
+        Hamburguer item2 = new Hamburguer();
+        System.out.println("Nome do 2º hambúrguer:");
+        item2.nome = scanner.nextLine();
+        System.out.println("Descrição:");
+        item2.descricao = scanner.nextLine();
+        System.out.println("Preço:");
+        item2.preco = scanner.nextDouble();
+        scanner.nextLine();
 
         Pedido pedido = new Pedido();
         System.out.println("Número do pedido:");
@@ -38,12 +32,8 @@ public class App {
         pedido.item2 = item2;
 
         pedido.exibirPedido();
-
-        System.out.println("Confirmar pagamento? (s/n)");
-        if (scanner.nextLine().equalsIgnoreCase("s")) {
-            pedido.pagar();
-            pedido.exibirPedido();
-        }
+        pedido.pagar();
+        pedido.exibirPedido();
 
         scanner.close();
     }
